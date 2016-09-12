34309e6d52ef7a3149e4f1974a3fe1f2b61729e5
oricalvo <ori.calvo@gmail.com>
2016-09-13 02:14:31 +0300

    Fixing test configuration

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,8 @@
-//var testsContext = require.context("../app", true, /_test$/);
-const testsContext = require.context('../app', true, /\.spec\.js$/)
+//
+//  require.context is a special webpack function
+//  it allows us to dynamically decide which files are considered dependency of this file and therefore will
+//  be processed by webpack
+//  In our case, we search for all app/**/*.spec.ts files
+//
+const testsContext = require.context('../app', true, /\.spec\.ts$/)
 testsContext.keys().forEach(testsContext);
